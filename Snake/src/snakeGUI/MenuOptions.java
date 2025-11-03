5d8c5464c2f56c4ff57182f33c13d49bbad44748
LucasAraya97 <arayalucas1997@gmail.com>
2025-11-02 23:51:02 -0300

    Agregue CardLayout

--- Snake/src/snakeGUI/MenuOptions.java
+++ Snake/src/snakeGUI/MenuOptions.java
@@ -1,8 +1,16 @@
 package snakeGUI;
 
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 public class MenuOptions extends JFrame {
+	private JPanel mainPanel;
+	private CardLayout cardLayout;
+	private GameScreen gameScreen;
     public MenuOptions() {
         setTitle("SnakeGame");
         setSize(500, 500);
@@ -10,38 +18,59 @@ public class MenuOptions extends JFrame {
         setResizable(false);
         setLocationRelativeTo(null);
         
-        JPanel panel = new JPanel();
-        panel.setLayout(null);
-        add(panel);
+        cardLayout = new CardLayout();
+        mainPanel =  new JPanel(cardLayout);
         
-        int centerX = (500 - 70) / 2;
+        //crear el panel del menu
+        JPanel menuPanel = createMenuPanel();
+        mainPanel.add(menuPanel, "menu");
         
-        // Botón de comienzo de juego (puedes posicionarlo también)
-        JButton startButton = new JButton("START");
-        startButton.setBounds(centerX, 200, 90, 30);  
-        startButton.setBorderPainted(true);
-        startButton.setContentAreaFilled(true);
-        startButton.setFocusPainted(true);
-        startButton.setOpaque(true);
-        // Agrega ActionListener aquí si quieres
-        
-        // Botón de exit
-        JButton exitButton = new JButton("EXIT");
-        exitButton.setBounds(centerX+10, 250, 70, 30);  // Centro aproximado en 500x500: x=215, y=235
-        exitButton.setBorderPainted(true);
-        exitButton.setContentAreaFilled(true);
-        exitButton.setFocusPainted(true);
-        exitButton.setOpaque(true);
-        exitButton.addActionListener(e -> System.exit(0));
-        
-        // Agregamos los botones al panel
-        panel.add(startButton);
-        panel.add(exitButton);
-        
+        add(mainPanel);
+        cardLayout.show(mainPanel,"menu");
+       
         setVisible(true);
     }
-    
+    private JPanel createMenuPanel() {
+    	JPanel panel = new JPanel();
+    	panel.setLayout(null);
+    	
+    	int centerX = (500 - 70) / 2;
+    	 
+    	 // Botón de comienzo de juego (puedes posicionarlo también)
+	    JButton startButton = new JButton("START");
+	    startButton.setBounds(centerX, 200, 90, 30);  
+	    startButton.setBorderPainted(true);
+	    startButton.setContentAreaFilled(true);
+	    startButton.setFocusPainted(true);
+	    startButton.setOpaque(true);
+	    startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Cambiar a la pantalla del juego
+                cardLayout.show(mainPanel, "Game");
+            }
+        });
+	    
+	    // Botón de exit
+	    JButton exitButton = new JButton("EXIT");
+	    exitButton.setBounds(centerX+10, 250, 70, 30);  // Centro aproximado en 500x500: x=215, y=235
+	    exitButton.setBorderPainted(true);
+	    exitButton.setContentAreaFilled(true);
+	    exitButton.setFocusPainted(true);
+	    exitButton.setOpaque(true);
+	    exitButton.addActionListener(e -> System.exit(0));
+	    
+	    // Agregamos los botones al panel
+	    panel.add(startButton);
+	    panel.add(exitButton);
+	    
+		return panel;
+    }
     public static void main(String[] args) {
-        new MenuOptions();
+    	SwingUtilities.invokeLater(() -> new MenuOptions());
+    	/*SwingUtilities.invokeLater asegura que el código dentro de él se ejecute 
+    	en el Event Dispatch Thread (EDT), que es el hilo dedicado de Swing para 
+    	manejar la UI de manera segura.*/
+    	
     }
 }
